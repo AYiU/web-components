3b80beaa3932b3db191f6313c5cf8586155a6850
AYiU <ayiu@ayiu.net>
2025-08-15 22:05:12 +0800

    Updated: padding in card

--- app/ui/card/page.tsx
+++ app/ui/card/page.tsx
@@ -4,6 +4,7 @@ export default function CardPage() {
   return (
     <>
       <MiCard
+        className="m-2"
         image="/images/dummy.png"
         title="Card Title"
         footer={<div>Footer Content</div>}
@@ -11,14 +12,14 @@ export default function CardPage() {
         Content
       </MiCard>
       <MiCard
-        image="/images/dummy.png"
+      className="m-2"
         title="Card Title"
         footer={<div>Footer Content</div>}
       >
         Content
       </MiCard>
       <a href="/">
-        <MiCard image="/images/dummy.png" title="Card Title">
+        <MiCard className="m-2" image="/images/dummy.png" title="Card Title">
           Content
         </MiCard>
       </a>
